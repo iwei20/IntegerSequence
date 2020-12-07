445fa6ec5ad92fd16e6f1beac43f6f0e5bd28362
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-12-07 12:43:00 -0500

    Add more rocketchat tests and adjust formatting

--- Tester.java
+++ Tester.java
@@ -21,13 +21,21 @@ public class Tester {
             }
         }
         System.out.println("You raised " + exceptionCounter + " exceptions and your product is " + product);
-    
-        IntegerSequence nr = new Range(10,20);
-        IntegerSequence as = new ArraySequence(nr);
+        
+        int[]nums = {1,3,5,0,-1,3,9};
+        IntegerSequence as = new ArraySequence(nums);
+
+        System.out.println("ArraySequence(array):");
+        while(as.hasNext()){
+            System.out.print(as.next() + (as.hasNext() ? ", " : "\n"));
+        }
+
+        r = new Range(10,20);
+        as = new ArraySequence(r);
     
         System.out.println("ArraySequence(seq):");
         while(as.hasNext()){
-            System.out.print(as.next()+", ");
+            System.out.print(as.next() + (as.hasNext() ? ", " : "\n"));
         }
         System.out.println();
     }
